2936a8bc310cbf4c7daa916239c533d6460bcab1
Debarghya Saha <72520073+debarghyaRONIN@users.noreply.github.com>
2024-11-15 13:39:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,15 +52,11 @@ submit1 = st.button("Tell Me About the Resume")
 submit2 = st.button("Percentage match")
 
 input_prompt1 = """
- You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
-  Please share your professional evaluation on whether the candidate's profile aligns with the role. 
- Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
+ You are a pdf reader who will jsonify the result and show
 """
 
 input_prompt2 = """
-You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-your task is to evaluate the resume against the provided job description. Give me the percentage of match if the resume matches
-the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+you will show the json file as a downloadable
 """
 
 if submit1:
